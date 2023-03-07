7a73f295c0032fae9be8d63f8d78760e700b2e30
masu <asukamasuuuu@gmail.com>
2023-03-07 15:34:23 +0800

    add timeout

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"sync"
+	"time"
 
 	"github.com/asukamasu/socks5"
 )
@@ -13,23 +15,27 @@ func main() {
 		"lisi":     "abde",
 	}
 
+	var mutex sync.Mutex
+
 	server := socks5.SOCKS5Server{
 		IP:   "172.19.64.1",
 		Port: 1080,
 		Config: &socks5.Config{
 			AuthMethod: socks5.MethodPassword,
 			PasswordChecker: func(username, password string) bool {
+				mutex.Lock()
+				defer mutex.Unlock()
 				wantpassword, ok := users[username]
 				if !ok {
 					return false
 				}
 				return wantpassword == password
 			},
+			TCPTimeout: 5 * time.Second,
 		},
 	}
 
-	err := server.Run()
-	if err != nil {
+	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
--- socks5.go
+++ socks5.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"time"
 )
 
 var (
@@ -34,6 +35,7 @@ type SOCKS5Server struct {
 type Config struct {
 	AuthMethod      Method
 	PasswordChecker func(username, password string) bool
+	TCPTimeout      time.Duration
 }
 
 func initConfig(config *Config) error {
@@ -48,37 +50,35 @@ func (s *SOCKS5Server) Run() error {
 	if err := initConfig(s.Config); err != nil {
 		return err
 	}
-
+	// Listen on the specifed IP:PORT
 	address := fmt.Sprintf("%s:%d", s.IP, s.Port)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
 	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("connect failure from %s: %s", conn.RemoteAddr(), err)
 			continue
 		}
+
 		go func() {
 			defer conn.Close()
-			err := handleConnection(conn, s.Config)
-			if err != nil {
+			if err := s.handleConnection(conn, s.Config); err != nil {
 				log.Printf("handle failure from %s: %s", conn.RemoteAddr(), err)
 			}
 		}()
 	}
 }
 
-func handleConnection(conn net.Conn, config *Config) error {
+func (s *SOCKS5Server) handleConnection(conn net.Conn, config *Config) error {
 	if err := auth(conn, config); err != nil {
 		return err
 	}
-	targetConn, err := request(conn)
-	if err != nil {
-		return err
-	}
-	return forward(conn, targetConn)
+	// Request phase
+	return s.request(conn)
 }
 
 func forward(conn io.ReadWriter, targetConn io.ReadWriteCloser) error {
@@ -88,30 +88,47 @@ func forward(conn io.ReadWriter, targetConn io.ReadWriteCloser) error {
 	return err
 }
 
-func request(conn io.ReadWriter) (io.ReadWriteCloser, error) {
+func (s *SOCKS5Server) request(conn io.ReadWriter) error {
 	message, err := NewClientRequestMessage(conn)
 	if err != nil {
-		return nil, err
-	}
-	// Check if the command supported
-	if message.Cmd != CmdConnect {
-		return nil, WriteRequestFailureMessage(conn, ReplyCommandNotSupported)
+		return err
 	}
 	// Check if the address type is	supported
-	if message.AddrType == IPv6Length {
-		return nil, WriteRequestFailureMessage(conn, ReplyAddressTypeNotSupported)
+	if message.AddrType == TypeIPv6 {
+		WriteRequestFailureMessage(conn, ReplyAddressTypeNotSupported)
+		return ErrAddressTypeNotSupported
 	}
+	// TCP service
+	if message.Cmd == CmdConnect {
+		return s.handleTCP(conn, message)
+	} else if message.Cmd == CmdUdp {
+		return s.handleUDP()
+	} else {
+		// Check if the command supported
+		WriteRequestFailureMessage(conn, ReplyCommandNotSupported)
+		return ErrCommandNotSupported
+	}
+}
 
+func (s *SOCKS5Server) handleUDP() error {
+	return nil
+}
+
+func (s *SOCKS5Server) handleTCP(conn io.ReadWriter, message *ClientRequestMessage) error {
 	address := fmt.Sprintf("%s:%d", message.Address, message.Port)
-	targetConn, err := net.Dial("tcp", address)
+	targetConn, err := net.DialTimeout("tcp", address, s.Config.TCPTimeout)
 	if err != nil {
-		return nil, WriteRequestFailureMessage(conn, ReplyConnectionRefused)
+		WriteRequestFailureMessage(conn, ReplyConnectionRefused)
+		return err
 	}
 
 	// Send success reply
 	addrValue := targetConn.LocalAddr()
 	addr := addrValue.(*net.TCPAddr)
-	return targetConn, WriteRequestSuccessMessage(conn, addr.IP, uint16(addr.Port))
+	if err := WriteRequestSuccessMessage(conn, addr.IP, uint16(addr.Port)); err != nil {
+		return err
+	}
+	return forward(conn, targetConn)
 }
 
 func auth(conn io.ReadWriter, config *Config) error {
